2d894c465efe266e84efd172f878f7050f896f3b
Lucas Sanz López <lucas.sanz.lopez@gmail.com>
2026-09-22 15:55:35 +0000

    hola

---
+++ main.py
@@ -0,0 +1,18 @@
+"Garaje" = ["Pintura", "Ventanas", "Ruedas", "Chasis"]
+"Pintura" = ["Color 1", "Color 2", "Color 3", "Color 4"]
+"Ruedas" = ["Tipo 1", "Tipo 2", "Tipo 3", "Tipo 4"]
+"Ruedas"[0] = {"existencias": 15, "precio": 100}
+"Ruedas"[1] = {"existencias": 15, "precio": 150}
+"Ruedas"[2] = {"existencias": 15, "precio": 200}
+"Ruedas"[3] = {"existencias": 15, "precio": 250}
+
+
+
+
+
+
+
+
+
+
+
