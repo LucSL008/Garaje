a04a2e7adb697f487449488ed08becc8dc6887dd
lunamdsn <lunabergmadsen@icloud.com>
2026-09-24 20:11:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,19 +8,19 @@ Ruedas[3] = {"existencias": 15, "precio": 250}
 
 Ventanas = ["Tipo 1", "Tipo 2", "Tipo 3", "Tipo 4"]
 Ventanas[0] = {"existencias": 10, "precio": 100}
-Ventanas[1] = {"existencias": 10, "precio": 115}
-Ventanas[2] = {"existencias": 10, "precio": 175}
-Ventanas[3] = {"existencias": 10, "precio": 200}
+Ventanas[1] = {"existencias": 10, "precio": 150}
+Ventanas[2] = {"existencias": 10, "precio": 200}
+Ventanas[3] = {"existencias": 10, "precio": 250}
 
 Pintura = ["Color 1", "Color 2", "Color 3", "Color 4"]
 Pintura[0] = {"existencias": 13, "precio": 100}
-Pintura[1] = {"existencias": 13, "precio": 120}
-Pintura[2] = {"existencias": 13, "precio": 150}
+Pintura[1] = {"existencias": 13, "precio": 150}
+Pintura[2] = {"existencias": 13, "precio": 200}
 Pintura[3] = {"existencias": 13, "precio": 250}
 
 Carroceria = ["Tipo 1", "Tipo 2", "Tipo 3", "Tipo 4"]
-Carroceria[0] = {"existencias": 12, "precio": 130}
-Carroceria[1] = {"existencias": 12, "precio": 170}
+Carroceria[0] = {"existencias": 12, "precio": 100}
+Carroceria[1] = {"existencias": 12, "precio": 150}
 Carroceria[2] = {"existencias": 12, "precio": 200}
 Carroceria[3] = {"existencias": 12, "precio": 250}
 
